a5f720386cd4c7a9785e34376206bcebe3d35ff5
Mauricio <conta.alonso@gmail.com>
2022-07-25 15:53:31 -0300

    Terminé ejercicio 1.

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -4,3 +4,19 @@ marca, modelo y si está encendido o apagado. Crea los métodos necesarios para
 permitir encender y apagar el auto.
 */
 
+let auto = {
+    //propiedades -> clave: valor
+    color: ['negro','blanco','plateado','rojo'],
+    marca: ['Chevrolet','Ford','Peugeout','Toyota','Nissan','Fiat','Volkswagen','Citröen','Renault'],
+    modelo:['urbano','sub-compacto','compacto','familiar','sedán','berlina','descapotable','coupé'],
+    encendido:false,
+    encender: ()=>{ document.write(`<p>Se encendió el vehículo.</p>`)},
+    apagar: ()=>{ document.write(`<p>Se apagó el vehículo.</p>`)}
+}
+   
+document.write(`<p>Colores de vehículos: ${auto.color}</p>`)
+document.write(`<p>Marcas de vehículos: ${auto.marca}</p>`)
+document.write(`<p>Modelos de vehículos: ${auto.modelo}</p>`)
+
+auto.encender();
+auto.apagar();
