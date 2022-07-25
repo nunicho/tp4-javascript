c85c5d6a0fa905dcb98bab90bee23f6272449720
Mauricio <conta.alonso@gmail.com>
2022-07-25 15:30:11 -0300

    Armado de la estructura HTML del TP4.

---
+++ js/ejercicio1.js
@@ -0,0 +1,6 @@
+/*
+1- Crea un objeto llamado auto que tenga algunas características como el color,
+marca, modelo y si está encendido o apagado. Crea los métodos necesarios para
+permitir encender y apagar el auto.
+*/
+
---
+++ js/ejercicio3.js
@@ -0,0 +1,5 @@
+/*
+3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las
+propiedades de alto y ancho, mas los métodos necesarios para modificar y
+mostrar sus propiedades, calcular el perímetro y el área
+*/
