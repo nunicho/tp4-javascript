42a8179012a279b27b654f4e6cf76bdcd1ef328a
Mauricio <conta.alonso@gmail.com>
2022-07-28 15:35:32 -0300

    Ajusté ejercicio 2 y terminé ejercicio 3.

---
+++ js/ejercicio2A.js
@@ -0,0 +1,145 @@
+/*
+CUENTA BANCARIA
+2-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+● Una propiedad titular con el valor "Alex".
+● Una propiedad saldo, teniendo como valor inicial 0.
+● Un método ingresar() que permita añadir dinero a la cuenta, pasando la
+cantidad como parámetro
+● Un método extraer() que permita retirar la cantidad pasada como
+parámetro.
+● Un método informar() que retorne la información del estado de la cuenta.
+Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a
+mostrar la descripción del estado de la cuenta.
+Rectángulos
+*/
+class Cuenta{
+    constructor(titular, saldo){
+        this.titular = titular;
+        this.saldo = saldo;
+        this.movimientos =['movimientos:']
+    }
+    ingresar(deposito){
+        if (isNaN(deposito) == false && deposito >=0){
+             this.saldo += deposito
+             this.movimientos.push(`Depósito: ${deposito}`)
+            alert(`Se depositó $${deposito}`) 
+        }
+        else{
+        alert("Lo ingresado no es una cifra válida.")
+    }       
+    }
+    extraer(retiro){
+        if (isNaN(retiro) == false && retiro >=0){
+             if(this.saldo >= retiro){
+                  this.saldo -= retiro;
+                  this.movimientos.push(`Retiro: ${retiro}`)
+                 alert(`Se retiró $${retiro}`)
+             }else{
+             alert(`Saldo insuficiente`)
+             }
+          }
+        else {
+            alert("Lo ingresado no es una cifra válida.")
+        }
+    }
+    informar(){
+            alert(`La cuenta de ${this.titular}, posee un saldo de $${this.saldo}`)
+        }
+    movimientosEnSesion(){
+            alert((`La cuenta de ${this.titular}, registró lo siguientes ${this.movimientos}`))
+    }
+    }
+
+let cliente = new Cuenta ('Alex',0,[''])
+
+while (confirm("Click Aceptar para operar con Rolling Bank") == true) {
+let eleccion =parseInt(prompt("Ingrese una opción: (1) Información de la cuenta (2) Depositar> (3)Extraer (4) Consultar movimientos"));
+switch (eleccion){
+case 1:
+    cliente.informar();  
+    break;
+case 2:
+    let deposito = parseFloat(prompt('Ingrese el monto deseado'))
+    cliente.ingresar(deposito);
+    cliente.informar();
+    break;
+case 3:
+    let retiro = parseFloat(prompt('Ingrese el monto a extraer'))
+    cliente.extraer(retiro)
+    cliente.informar();
+case 4:
+    cliente.movimientosEnSesion()
+    break;
+    default:
+        alert('Ingresaste una opción errónea')
+}
+}
+alert("Gracias por operar con Rolling Bank. Se imprimirá su ticket")
+document.write(cliente.movimientosEnSesion)
+
+
+/*
+
+HACER UN MODELO DE TICKET
+
+*/
+
+/*
+class Cuenta{
+    constructor(titular, saldo){
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+    ingresar(deposito){
+        if (isNaN(deposito) == false && deposito >=0){
+             this.saldo += deposito
+            alert(`Se depositó $${deposito}`) 
+        }
+        else{
+        alert("Lo ingresado no es una cifra válida.")
+    }       
+    }
+    extraer(retiro){
+        if (isNaN(retiro) == false && retiro >=0){
+             if(this.saldo >= retiro){
+                  this.saldo -= retiro;
+                 alert(`Se retiró $${retiro}`)
+             }else{
+             alert(`Saldo insuficiente`)
+             }
+          }
+        else {
+            alert("Lo ingresado no es una cifra válida.")
+        }
+    }
+    informar(){
+            alert(`La cuenta de ${this.titular}, posee un saldo de $${this.saldo}`)
+        }
+    }
+
+let cliente = new Cuenta ('Alex',0)
+
+while (confirm("Click Aceptar para operar con Rolling Bank") == true) {
+let eleccion =parseInt(prompt("Ingrese una opción: (1) Información de la cuenta (2) Depositar> (3)Extraer"));
+switch (eleccion){
+case 1:
+    cliente.informar(cliente);  
+    break;
+case 2:
+    let deposito = parseFloat(prompt('Ingrese el monto deseado'))
+    cliente.ingresar(deposito);
+    cliente.informar();
+    break;
+case 3:
+    let retiro = parseFloat(prompt('Ingrese el monto a extraer'))
+    cliente.extraer(retiro)
+    cliente.informar();
+    break;
+    default:
+        alert('Ingresaste una opción errónea')
+}
+}
+alert("Gracias por operar con Rolling Bank. Se imprimirá su ticket")
+*/
+
+
--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -3,3 +3,60 @@
 propiedades de alto y ancho, mas los métodos necesarios para modificar y
 mostrar sus propiedades, calcular el perímetro y el área
 */
+
+class Rectangulos{ 
+
+    constructor(nombre, ancho, alto, perimetro, area){
+        this.nombre= nombre;   
+        this.ancho = ancho;
+        this.alto = alto;
+        this.perimetro=perimetro;
+        this.area=area;
+
+    }
+
+    ingresarAlto(altura){
+        altura=parseInt(prompt("Ingresar valor para Alto"))
+                 this.alto += altura
+    }
+    ingresarAncho(anchura){
+        anchura=parseInt(prompt("Ingresar valor para Ancho"))
+                  this.ancho += anchura
+    }
+    calculos(){
+            this.perimetro += 2*(this.ancho+this.alto)
+            this.area += this.ancho*this.alto 
+    
+    }
+        mostrarDatos(){
+        document.write(`<ul>
+        <li>Nombre: ${this.nombre}</li>
+        <li>Ancho: ${this.ancho}</li>
+        <li>Alto: ${this.alto}</li>
+        <li>Perímetro: ${this.perimetro}</li>
+        <li>Área: ${this.area}</li>
+        </ul>   ` )
+    }
+
+}
+
+let rectangulo1 = new Rectangulos('Rectangulo1',0,0,0,0,)
+rectangulo1.ingresarAlto();
+rectangulo1.ingresarAncho();
+rectangulo1.calculos()
+
+
+let rectangulo2 = new Rectangulos('Rectangulo2',0,0,0,0,)
+rectangulo2.ingresarAlto();
+rectangulo2.ingresarAncho();
+rectangulo2.calculos()
+
+let rectangulo3 = new Rectangulos('Rectangulo2',0,0,0,0,)
+rectangulo3.ingresarAlto();
+rectangulo3.ingresarAncho();
+rectangulo3.calculos()
+
+rectangulo1.mostrarDatos();
+rectangulo2.mostrarDatos();
+rectangulo3.mostrarDatos();
+
